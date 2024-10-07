d21cbb39e574db6d04dd169682e42538e8c16c0b
NicolasPag112 <nicolaspag112@gmail.com>
2024-10-07 15:31:07 -0300

    Controller de tarefa criada

---
+++ TarefasBackEnd/Controllers/TarefaController.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TarefasBackEnd.Controllers
+{
+    [ApiController]
+    [Route("tarefa")]
+    public class TarefaController : ControllerBase
+    {
+        
+    }
+}
