f6d4a4db8263fb4ba7f3066b753b4181aa152937
NicolasPag112 <nicolaspag112@gmail.com>
2024-10-10 12:28:01 -0300

    Atualização no Program.cs

--- TarefasBackEnd/Program.cs
+++ TarefasBackEnd/Program.cs
@@ -1,5 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TarefasBackEnd.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>options.UseInMemoryDatabase("DBTarefas"));
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
