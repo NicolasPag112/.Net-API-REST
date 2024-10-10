90bd5da9c3426ae6c020e94761ff2bf776aa3ffa
NicolasPag112 <nicolaspag112@gmail.com>
2024-10-10 13:27:38 -0300

    Atualização do Program.cs

--- TarefasBackEnd/Program.cs
+++ TarefasBackEnd/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>options.UseInMemoryDatabase("DBTarefas"));
+builder.Services.AddTransient<ITarefaRepository, TarefaRepository>();
 
 var app = builder.Build();
 
