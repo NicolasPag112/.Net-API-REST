237b82aa85396c1b5923be55f4452acd95c95dd4
NicolasPag112 <nicolaspag112@gmail.com>
2024-10-31 15:47:44 -0300

    Autenticação adicionada ao startar o programa

--- TarefasBackEnd/Program.cs
+++ TarefasBackEnd/Program.cs
@@ -1,9 +1,31 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using TarefasBackEnd.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+var key = Encoding.ASCII.GetBytes("tokenSuperExtraGrandePorqueOPrimeiroEraMuitoPequenoEDeuErroCursoUdemy");
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}
+).AddJwtBearer(options => {
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters{
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 builder.Services.AddDbContext<DataContext>(options =>options.UseInMemoryDatabase("DBTarefas"));
 builder.Services.AddTransient<ITarefaRepository, TarefaRepository>();
 builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
