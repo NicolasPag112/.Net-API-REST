1a9a1b28104fccc2f6298b5522f1d570cfc7ad95
NicolasPag112 <nicolaspag112@gmail.com>
2024-10-31 15:34:28 -0300

    Geração de token adicionada

--- TarefasBackEnd/Controllers/UsuarioController.cs
+++ TarefasBackEnd/Controllers/UsuarioController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using TarefasBackEnd.Model;
 using TarefasBackEnd.Model.ViewModels;
 using TarefasBackEnd.Repositories;
@@ -38,8 +42,30 @@ namespace TarefasBackEnd.Controllers
             usuario.Senha = "";
 
             return Ok(new {
-                usuario = usuario
+                usuario = usuario,
+                token = GenerateToken(usuario)
             });
+        }
+
+        private string GenerateToken(Usuario usuario)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes("tokenSuperExtraGrandePorqueOPrimeiroEraMuitoPequenoEDeuErroCursoUdemy");
+
+            var descriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, usuario.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(descriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
